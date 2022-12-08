5ae841c46a3cd56d6fdc9090e60ff4c4da05dac5
Merel van der Wal <Merelvanderwal94@gmail.com>
2022-12-08 14:58:07 +0100

    made sure it can still make

--- src/initialize/init_player.c
+++ src/initialize/init_player.c
@@ -6,7 +6,7 @@
 /*   By: mevan-de <mevan-de@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/12/06 13:39:40 by mevan-de      #+#    #+#                 */
-/*   Updated: 2022/12/06 13:40:47 by mevan-de      ########   odam.nl         */
+/*   Updated: 2022/12/08 14:57:14 by mevan-de      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,6 @@
 
 void	init_player_vars(t_player *player_data, char **map)
 {
-	player_data->position.x
+	player_data->position.x = 0;
+	(void) map;
 }
