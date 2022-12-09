4fadf81998a63043737cc2845396a01d0cbf0cf5
Merel van der Wal <Merelvanderwal94@gmail.com>
2022-12-09 13:21:01 +0100

    some restructuring, nothing major

--- src/rendering/render_loop.c
+++ src/rendering/render_loop.c
@@ -1,12 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        ::::::::            */
-/*   mlx_loop.c                                         :+:    :+:            */
+/*   render_loop.c                                      :+:    :+:            */
 /*                                                     +:+                    */
 /*   By: mevan-de <mevan-de@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/12/09 12:02:12 by mevan-de      #+#    #+#                 */
-/*   Updated: 2022/12/09 12:02:13 by mevan-de      ########   odam.nl         */
+/*   Updated: 2022/12/09 12:11:11 by mevan-de      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "cub3d_utils.h"
+#include "cub3d_MLX.h"
+#include "cub3d_structs.h"
+#include "libft.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+
+void	render_loop(void *data)
+{
+	t_cub3d	*cub3d_data;
+
+	cub3d_data = data;
+	update_values()
+}
